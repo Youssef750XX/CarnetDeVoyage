29db68bee4d34130f8905a768400f3d54d041a70
Youssef Bendjebbour <bendjebbouryoussef@hotmail.fr>
2026-06-01 12:17:39 +0200

    Debut du projet

--- Projet Voyage/ContentView.swift
+++ Projet Voyage/ContentView.swift
@@ -9,15 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Text("Youssef").font(.largeTitle)
+        Text("Bendjebbour").font(.largeTitle,)
+        Image(systemName: "heart.fill").imageScale(.large).foregroundColor(.red)
+            .font(.system(size: 50))
+        
         }
-        .padding()
     }
-}
 
 #Preview {
     ContentView()
