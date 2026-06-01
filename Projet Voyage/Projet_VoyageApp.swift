068969260d3d792b47c5e44fd7dfb4fd778cb7a1
Youssef Bendjebbour <bendjebbouryoussef@hotmail.fr>
2026-06-01 10:51:31 +0200

    Initial Commit

---
+++ Projet Voyage/Projet_VoyageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Projet_VoyageApp.swift
+//  Projet Voyage
+//
+//  Created by apprenant134 on 01/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct Projet_VoyageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
